c2f7f418898d7f469929fb20699db67db178c69d
Simon Thorell <kontakt@simonthorell.se>
2024-02-05 20:49:21 +0000

    Add MQTT event handler

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,12 @@
 #include "hash_generator.h"
 #include "sqlite_database.h"
 
+// TODO: Replace with secrets from a secure location
+#define MQTT_BROKER_URI "tcp://localhost:1883"
+#define MQTT_CLIENT_ID "client1"
+#define MQTT_TOPIC "test/topic"
+#define MQTT_QOS 1
+
 int main() {
     // Create a new database
     SQLiteDatabase db("sql_database.db");
@@ -21,38 +27,47 @@ int main() {
         return -1;
     }
 
-    // TODO: Subscribe to the MQTT "credentials" topic
+    // Initialize the MQTTHandler
+    MQTTHandler mqtt(MQTT_BROKER_URI, MQTT_CLIENT_ID, MQTT_TOPIC, MQTT_QOS);
 
-    // TODO: Parse the JSON message from the MQTT topic
+    mqtt.connect();    // Connect to the MQTT broker
+    mqtt.subscribe();  // Subscribe to the topic
 
-    // TODO: Decrypt the MQTT message using AES256 Decryptor
+    // Wait for messages to arrive and handle here...
+    while (true) {
+        // TODO: Parse the JSON message from the MQTT topic
+        // TODO: Decrypt email & password using AES256 Decryptor
 
-    // Dummy user data (Replace with decrypted data from MQTT message)
-    std::string email = "user@example.com";
-    std::string password = "password"; // Store a hashed password, not plain text
+        // Dummy user data (Replace with decrypted data from MQTT message)
+        std::string email = "user@example.com";
+        std::string password = "password"; // Store a hashed password, not plain text
 
-    // Generate a secure, random salt for each password
-    SaltGenerator saltGenerator;
-    std::string salt = saltGenerator.generateSalt(12);
+        // Generate a secure, random salt for each password
+        SaltGenerator saltGenerator;
+        std::string salt = saltGenerator.generateSalt(12);
 
-    // Salt & Hash the password
-    HashGenerator hashGenerator;
-    std::string saltedPassword = password + salt;
-    std::string hashedPassword = hashGenerator.computeSHA256(saltedPassword);
+        // Salt & Hash the password
+        HashGenerator hashGenerator;
+        std::string saltedPassword = password + salt;
+        std::string hashedPassword = hashGenerator.computeSHA256(saltedPassword);
 
-    // Insert user data into the users table
-    if (!db.insert("users", "email, password, salt", "'" + email + "', '" + hashedPassword + "', '" + salt + "'")) {
-        std::cerr << "Data insertion failed!" << std::endl;
-        return -1;
+        // Insert user data into the users table
+        if (!db.insert("users", "email, password, salt", "'" + email + "', '" + hashedPassword + "', '" + salt + "'")) {
+            std::cerr << "Data insertion failed!" << std::endl;
+            return -1;
+        }
     }
 
-    // Select and print all entries from the "users" table
+    // Print all data from the users table in SQL database
     if (!db.selectAllFromTable("users")) {
         std::cerr << "Failed to select data!" << std::endl;
         return -1;
     }
 
-    // Close the database
+    // Disconnect from the MQTT broker
+    mqtt.disconnect();
+
+    // Close the SQL database
     db.close();
 
     return EXIT_SUCCESS;
