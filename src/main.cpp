5fd81abf10a69c3108b24bec7fbfdbba672375ed
Simon Thorell <kontakt@simonthorell.se>
2024-02-05 18:20:00 +0000

    Dependencies add CMake

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <openssl/evp.h> // OpenSSL Crypto library
+#include <sqlite3.h>     // SQLite3 library
+#include <mqtt/client.h> // MQTT library
+#include "test_file.h"
 
 int main() {
 
+    int testing = test_method();
+    std::cout << "Testing: " << testing << std::endl;
     return 0;
 }
